6a9994161659d8f641bc5f024a7c178610c1b222
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-09-08 19:37:42 -0400

    if user attempts to register with a username that already exists, recieves user already exists error

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -3,15 +3,14 @@ import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
 import {
-    ADD_CONTACT,
-    DELETE_CONTACT,
-    SET_CURRENT,
-    CLEAR_CURRENT,
-    UPDATE_CONTACT,
-    FILTER_CONTACTS,
-    CLEAR_FILTER,
     REGISTER_SUCCESS,
-    REGISTER_FAIL
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT,
+    CLEAR_ERRORS
 } from '../types';
 
 const AuthState = props => {
@@ -58,7 +57,7 @@ const AuthState = props => {
     const logout = () => console.log('logout')
 
     // Clear Errors
-    const clearErrors = () => console.log('clearErrors')
+    const clearErrors = () => dispatch({ type: CLEAR_ERRORS })
 
     return (
         <AuthContext.Provider 
