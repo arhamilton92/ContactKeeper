5fd414d2837305dfb7cb05c3985dda5709795a82
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-09-08 19:59:29 -0400

    user is logged in/authenticated when registers to website.

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -2,6 +2,7 @@ import React, { useReducer } from 'react';
 import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
+import setAuthToken from  '../../utils/setAuthToken'
 import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
@@ -25,7 +26,22 @@ const AuthState = props => {
     const [state, dispatch] = useReducer(authReducer, initialState)
 
     // Load User
-    const loadUser = () => console.log('loaduser');
+    const loadUser = async () => {
+        if (localStorage.token) {
+            setAuthToken(localStorage.token)
+        }
+
+        try {
+            const res = await axios.get('/api/auth');
+
+            dispatch({ 
+                type: USER_LOADED, 
+                payload: res.data 
+            })
+        } catch (error) {
+            dispatch({ type: AUTH_ERROR})
+        }
+    }
 
     // Regsiter User
     const register = async formData => {
